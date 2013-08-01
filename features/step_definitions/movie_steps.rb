aa2a4fda743357d890f72e2b7d29b4854f2fc92a
chuntaolu <chuntaolu13@gmail.com>
2013-08-01 18:27:30 -0400

    Finish hw3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie.symbolize_keys)
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
+  regex = /#{e1}.*#{e2}/m
+  page.body.should =~ regex
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +28,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(', ')
+  ratings.each do |rating|
+    if uncheck == 'un'
+      step %Q{I uncheck "ratings_#{rating}"}
+    else
+      step %Q{I check "ratings_#{rating}"}
+    end
+  end
+end
+
+
+Then /I should see all of the movies/ do
+  #number of table rows include table head
+  page.all("table#movies tr").count.should == 11
 end
